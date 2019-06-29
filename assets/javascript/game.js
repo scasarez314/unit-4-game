d4a11d85d58b6b5364598d967fd72a82e572732b
Sierra Casarez <scasarez314@gmail.com>
2019-06-29 18:06:51 -0500

    started pseudocoding the Javascript, added a few css properties and linked the jQuery script in the head

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+// have the computer generate a random number and show on screen.
+
+// set up the images to have a random number, also shuffle a random set of number upon lossing, winning, and refreshing.
+// track the wins and losses and show on screen. 
+// track the clicks on the crystals to add on the score.
