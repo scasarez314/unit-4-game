ec9401fa8e1bf936f2e677b4266f33383c93ad1f
Sierra Casarez <scasarez314@gmail.com>
2019-06-30 16:35:54 -0500

    the picture clicks now update to the screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,66 +1,73 @@
 $(document).ready(function (ev) {
+    initalStart();
 
     // have the computer generate a random number.
     computerGuess = Math.floor(Math.random() * 102 + 19);
-    console.log(computerGuess + "this is the computers random number");
+    // console.log(computerGuess + "this is the computers random number");
 
     $("#generatedNumber").text(computerGuess);
 
 
     // track gem values 
-    var gem1 = "";
-    var gem2 = "";
-    var gem3 = "";
-    var gem4 = "";
-    var isGemOneClicked = false;
-    var score = 0;
-    var newScore = "";
-    $("#playerScore").text(score + newScore);
+    var newScore = 0;
 
-    var randonGemNumber = Math.floor(Math.random() * 12 + 1);
+    $("#playerScore").text(newScore);
 
+
+    function randonGemNumber() {
+        var random = Math.floor(Math.random() * 12 + 1);
+        console.log(random);
+        return random;
+
+    }
+    function initalStart() {
+        gem1 = randonGemNumber();
+        gem2 = randonGemNumber();
+        gem3 = randonGemNumber();
+        gem4 = randonGemNumber();
+        newScore = 0;
+        console.log(gem1 + "heres gem1")
+    }
     // Gem1 on click functions
     $(".gem1").on("click", function (event) {
 
-        var gem1Value = randonGemNumber
-        gem1 = parseInt(gem1)
-        newScore = parseInt(newScore)
-        if (isGemOneClicked === true) {
+        newScore = gem1 + newScore
+        var answer = newScore;
 
-            answer = (gem1Value + score + newScore);
-            $("#playerScore").val(answer)
-        }
+        $("#playerScore").text(answer);
 
-
-        console.log($(this).val())
+        console.log(answer + "answer")
     });
+
 
     // Gem2 onclick functions
     $(".gem2").on("click", function (event) {
 
-        var gem2 = Math.floor(Math.random() * 12 + 1);
-        $(".gem2").val(gem2)
-        console.log(gem2)
-    })
 
-    // Gem2 onclick functions
-    var gem3 = Math.floor(Math.random() * 12 + 1);
-    $(".gem3").val(gem3)
-    console.log(gem3)
+        $(".gem2").val(gem2)
+        console.log(gem2 + "" + newScore)
+    });
+
+    // Gem3 onclick functions
+    $(".gem3").on("click", function (event) {
+
+        var gem3 = Math.floor(Math.random() * 12 + 1);
+        $(".gem3").val(gem3)
+        console.log(gem3 + "" + "gem3 value")
+    });
+
 
     // Gem4 onclick functions 
-    var gem4 = Math.floor(Math.random() * 12 + 1);
-    $(".gem4").val(gem4)
-    console.log(gem4)
+    $(".gem4").on("click", function (event) {
+
+        var gem4 = Math.floor(Math.random() * 12 + 1);
+        $(".gem4").val(gem4)
+        console.log(gem4 + "" + "gem4 value")
+
+    })
+
 
     // track the wins and losses and show on screen.
     var Wins = 0;
     var Losses = 0;
-})
-
-
-
-
-
-
-
+});
